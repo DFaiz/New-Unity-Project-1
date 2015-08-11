e8fe19b3186729c123ac81a7d5d1c2bcc838d65e
DFaiz <maysam815@gmail.com>
2015-08-11 22:01:10 +0300

    Added panels

--- Assets/Scripts/UI_Script.cs
+++ Assets/Scripts/UI_Script.cs
@@ -5,11 +5,11 @@ using System.Collections;
  
 public class UI_Script : MonoBehaviour 
 {
-	//All canvas objects
-	public Canvas CanvasMain_Menu;
-	public Canvas CanvasSInfo;
-	public Canvas CanvasMulti;
-	public Canvas CanvasOptions;
+	//public GameObject MainPanel;
+	public GameObject MainPanel;
+	public GameObject OptionsPanel;
+	public GameObject MultiPanel;
+	public GameObject InfoPanel;
 
 	//Main menu elements
 	public Button ButtonSINGLEPLAYER;
@@ -28,66 +28,62 @@ public class UI_Script : MonoBehaviour
     {
 		Debug.Log ("load menu");
 
-		//Initialize the canvas objects
-		CanvasMain_Menu = CanvasMain_Menu.GetComponent<Canvas>();
-		CanvasSInfo = CanvasSInfo.GetComponent<Canvas>();
-		CanvasMulti = CanvasMulti.GetComponent<Canvas>();
-		CanvasOptions = CanvasOptions.GetComponent<Canvas>();
+		MainPanel.SetActive (true);
+		InfoPanel.SetActive (false);
+		MultiPanel.SetActive (false);
+		OptionsPanel.SetActive (false);
 
 		//Initialize buttons
 		ButtonSINGLEPLAYER = ButtonSINGLEPLAYER.GetComponent<Button> ();
 		ButtonMULTIPLAYER = ButtonMULTIPLAYER.GetComponent<Button> ();
 		ButtonSTUDENTINFO = ButtonSTUDENTINFO.GetComponent<Button> ();
 		ButtonOPTIONS = ButtonOPTIONS.GetComponent<Button> ();
-		ButtonBACK = ButtonBACK.GetComponent<Button>();
 		ButtonQUIT = ButtonQUIT.GetComponent<Button>();
+		ButtonBACK = ButtonBACK.GetComponent<Button>();
 		ButtonMPlay = ButtonMPlay.GetComponent<Button>();
-
-		//Hide irelevant canvases
-		CanvasMain_Menu.enabled = true;
-		CanvasSInfo.enabled = false;
-		CanvasMulti.enabled = false;
-		CanvasOptions.enabled = false;
     }
  
     public void SinglePlayerPress() //This function will be used on our Singleplayer button
     {
 		Debug.Log ("SinglePlayerPress");
-		CanvasMain_Menu.enabled = false; //disable the main menu
+		//CanvasMain_Menu.enabled = false; //disable the main menu
 		//StartSinglePlayerLevel ();
     }
  
 	public void MultiPlayerPress() //This function will be used on our Multiplayer button
 	{
 		Debug.Log ("MultiPlayerPress");
-		CanvasMain_Menu.enabled = false; //disable the main menu
-		CanvasMulti.enabled = true; //enable the multiplayer canvas
+		MultiPanel.SetActive (false);
+		MultiPanel.SetActive (true);
+
+		//CanvasMain_Menu.enabled = false; //disable the main menu
+		//CanvasMulti.enabled = true; //enable the multiplayer canvas
 		//StartMultiPlayerLevel ();
 	}
 
 	public void StudentInfoPress() //This function will be used on our Student Info button
 	{
 		Debug.Log ("StudentInfoPress");
-		CanvasMain_Menu.enabled = false; //disable the main menu
-		CanvasSInfo.enabled = true;	//enable the student info canvas
+		//CanvasMain_Menu.enabled = false; //disable the main menu
+		//CanvasSInfo.enabled = true;	//enable the student info canvas
 	}
 
 	public void BackPress() //This function will be used on our Back button
 	{
 		Debug.Log ("BackPress");
-		CanvasMain_Menu.enabled = true; //enable the main menu
-		CanvasSInfo.enabled = false; // disable the student info canvas
-		CanvasMulti.enabled = false; // disable the multiplayer canvas
-		CanvasOptions.enabled = false; // disable the options canvas
+		//CanvasMain_Menu.enabled = true; //enable the main menu
+		//CanvasSInfo.enabled = false; // disable the student info canvas
+		//CanvasMulti.enabled = false; // disable the multiplayer canvas
+		//CanvasOptions.enabled = false; // disable the options canvas
 	}
 
 	public void OptionsPress() //This function will be used on our Options Info button
 	{
 		Debug.Log ("OptionsPress");
-		CanvasMain_Menu.enabled = false; //disable the main menu
-		CanvasMulti.enabled = false; // disable the multiplayer canvas
-		CanvasSInfo.enabled = false; // disable the student info canvas
-		CanvasOptions.enabled = true; //enable the options canvas
+		//CanvasMain_Menu.enabled = false; //disable the main menu
+		//CanvasMulti.enabled = false; // disable the multiplayer canvas
+		//CanvasSInfo.enabled = false; // disable the student info canvas
+		///CanvasOptions.enabled = true; //enable the options canvas
 	}
 
     public void StartSinglePlayerLevel () //This function will be used on our Play button
