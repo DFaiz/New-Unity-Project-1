4a4e7fd3e0c31d15a2cc2240056e66c7e49321be
balashik <galshalit@gmail.com>
2015-08-14 17:54:59 +0300

    gui updates

--- Assets/Scripts/UI_Script.cs
+++ Assets/Scripts/UI_Script.cs
@@ -28,42 +28,13 @@ public class UI_Script : MonoBehaviour
     void Start ()
     {
 		Debug.Log ("load menu");
-
-		MainPanel.SetActive (true);
-		//InfoPanel.SetActive (false);
-		//MultiPanel.SetActive (false);
-		//OptionsPanel.SetActive (false);
-
-		//Initialize buttons
-		ButtonSINGLEPLAYER = ButtonSINGLEPLAYER.GetComponent<Button> ();
-		ButtonMULTIPLAYER = ButtonMULTIPLAYER.GetComponent<Button> ();
-		ButtonSTUDENTINFO = ButtonSTUDENTINFO.GetComponent<Button> ();
-		ButtonOPTIONS = ButtonOPTIONS.GetComponent<Button> ();
-		ButtonQUIT = ButtonQUIT.GetComponent<Button>();
-		ButtonBACK = ButtonBACK.GetComponent<Button>();
-		ButtonMPlay = ButtonMPlay.GetComponent<Button>();
-
-		Debug.Log(ParentObject.transform.GetChild (0).name.ToString ());
-		GameObject main = ParentObject.transform.GetChild (0).gameObject;
-		ParentObject.transform.GetChild (0).gameObject.SetActive (true);
-
-//		foreach (GameObject child in main.get) {
-//			Debug.Log(child.gameObject.name.ToString());
-//					child.gameObject.SetActive (true);		
-//				}
-//
-		//Debug.Log(ParentObject.transform.GetChild (1).name.ToString ());
-		//ParentObject.transform.GetChild (1).gameObject.SetActive (false);
-
-		//Debug.Log(ParentObject.transform.GetChild (2).name.ToString ());
-		//ParentObject.transform.GetChild (2).gameObject.SetActive (false);
+	
     }
  
     public void SinglePlayerPress() //This function will be used on our Singleplayer button
     {
 		Debug.Log ("SinglePlayerPress");
-		//CanvasMain_Menu.enabled = false; //disable the main menu
-		//StartSinglePlayerLevel ();
+
     }
  
 	public void MultiPlayerPress() //This function will be used on our Multiplayer button
@@ -71,10 +42,7 @@ public class UI_Script : MonoBehaviour
 		Debug.Log ("MultiPlayerPress");
 		MainPanel.SetActive (false);
 		MultiPanel.SetActive (true);
-
-		//CanvasMain_Menu.enabled = false; //disable the main menu
-		//CanvasMulti.enabled = true; //enable the multiplayer canvas
-		//StartMultiPlayerLevel ();
+		
 	}
 
 	public void StudentInfoPress() //This function will be used on our Student Info button
@@ -83,26 +51,22 @@ public class UI_Script : MonoBehaviour
 		MainPanel.SetActive (false);
 		InfoPanel.SetActive (true);
 
-		//CanvasMain_Menu.enabled = false; //disable the main menu
-		//CanvasSInfo.enabled = true;	//enable the student info canvas
+
 	}
 
-	public void BackPress() //This function will be used on our Back button
+	public void BackPress(GameObject backFromPanel) //This function will be used on our Back button
 	{
 		Debug.Log ("BackPress");
-		//CanvasMain_Menu.enabled = true; //enable the main menu
-		//CanvasSInfo.enabled = false; // disable the student info canvas
-		//CanvasMulti.enabled = false; // disable the multiplayer canvas
-		//CanvasOptions.enabled = false; // disable the options canvas
+		backFromPanel.SetActive (false);
+		MainPanel.SetActive (true);
 	}
 
 	public void OptionsPress() //This function will be used on our Options Info button
 	{
 		Debug.Log ("OptionsPress");
-		//CanvasMain_Menu.enabled = false; //disable the main menu
-		//CanvasMulti.enabled = false; // disable the multiplayer canvas
-		//CanvasSInfo.enabled = false; // disable the student info canvas
-		///CanvasOptions.enabled = true; //enable the options canvas
+		MainPanel.SetActive (false);
+		OptionsPanel.SetActive (true);
+
 	}
 
     public void StartSinglePlayerLevel () //This function will be used on our Play button
